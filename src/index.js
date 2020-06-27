3f0c643ae1de2622e74ff17933cf7b96d09ac7d0
hladkky <hladkky@gmail.com>
2020-06-27 18:10:14 +0300

    css var

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.5.3/lottie.js"
 require('typeface-montserrat');
 
 ReactDOM.render(<App />, document.getElementById('root'));
